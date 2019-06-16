9358e15989c87c2bba0d2b90c13ce50cb17759e1
Sho Tanaka <twinkle.over.the.sky@gmail.com>
2019-06-16 22:42:21 +0900

    add abc128

---
+++ abc128/abc128_c.py
@@ -0,0 +1,20 @@
+import math 
+import numpy as np
+
+N, M = map(int, input().split())
+k = []
+p = []
+for i in range(M):
+    k.append(int, input().split())
+
+p = input().split()
+print(k, p)
+
+
+'''
+2 2
+2 1 2
+1 2
+0 1
+
+'''
