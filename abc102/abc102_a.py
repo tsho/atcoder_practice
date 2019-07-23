3c2068e9f72451b60b5d8ee8caf83e076397d36b
Sho Tanaka <twinkle.over.the.sky@gmail.com>
2019-07-24 00:11:27 +0900

    Add abc102 a

---
+++ abc102/abc102_a.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+for i in range(N, (N+1)*100, N):
+    if i % 2 == 0 and i % N == 0:
+        break
+    print(i)
+
+print(i)
+
+
+'''
+3
+6
+
+10
+10
+
+999999999
+1999999998
+'''
