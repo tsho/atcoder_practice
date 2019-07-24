c5c20a53ff85b1793ed4f9ad9c1752d9a5f8d366
Sho Tanaka <twinkle.over.the.sky@gmail.com>
2019-07-24 23:39:54 +0900

    Add abc105 a

---
+++ abc105/abc105_a.py
@@ -0,0 +1,20 @@
+N, K = map(int, input().split())
+
+
+if N % K == 0:
+    print(0)
+else:
+    print(1)
+
+'''
+https://atcoder.jp/contests/abc105/tasks/abc105_a
+
+7 3
+1
+
+100 10
+0
+
+1 1
+0
+'''
